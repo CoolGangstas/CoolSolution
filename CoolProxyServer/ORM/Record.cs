582a6836dc092c6c595cbc74caceac059ae47faf
Alex Budnitsky <alex.budnitsky1997@gmail.com>
2017-04-11 11:55:14 +0300

    add ORM configuration

---
+++ CoolProxyServer/ORM/Record.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// The record.
+    /// </summary>
+    public class Record
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cloud_id.
+        /// </summary>
+        public int CloudId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user id.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether is completed.
+        /// </summary>
+        public bool IsCompleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user.
+        /// </summary>
+        public User User { get; set; }
+    }
+}
